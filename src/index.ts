b7e1b235953f9c2ede1ea633cf5fcd589d62aa9b
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-01-06 19:38:51 +0300

    init: prisma init

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import express, { Application, Request, Response } from "express";
-import bodyParser from "body-parser";
+import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app: Application = express();
-
+app.use(cors({ origin: '<http://localhost:3000>' }));
 app.use(bodyParser.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("hello world");
+app.get('/', (req: Request, res: Response) => {
+  res.send('hello world');
 });
 
 app.listen(PORT, () => {
